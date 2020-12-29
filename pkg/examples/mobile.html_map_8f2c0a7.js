1a33baddac99a5c6d4f18e80759465579a46f22e
liaoxuezhi <liaoxuezhi@icloud.com>
2020-12-29 11:02:45 +0000

    自动同步 gh-pages

--- pkg/examples/mobile.html_map_8f2c0a7.js
+++ pkg/examples/mobile.html_map_8f2c0a7.js
@@ -8727,7 +8727,7 @@ amis.require.resourceMap({
       "type": "js"
     },
     "p7": {
-      "url": "/amis/pkg/app_f0ac6d3.js",
+      "url": "/amis/pkg/app_f7560ce.js",
       "type": "js"
     },
     "p1": {
